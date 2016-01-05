1e6a8796fe420b5299e94480a23879644ac2fc53
ROHIT GUPTA <rohitgupta@ROHITs-MacBook-Pro.local>
2016-01-05 13:47:17 -0800

    added Flickr controller and search file

---
+++ VirtualTourist/FlickrPhotosViewController.swift
@@ -0,0 +1,26 @@
+//
+//  FlickrPhotosViewController.swift
+//  VirtualTourist
+//
+//  Created by ROHIT GUPTA on 12/30/15.
+//  Copyright © 2015 ROHIT GUPTA. All rights reserved.
+//
+
+import UIKit
+
+class FlickrPhotosViewController: UIViewController {
+    
+    private let reuseIdentifier = "FlickrCell"
+    private let sectionInsets = UIEdgeInsets(top: 50.0, left: 20.0, bottom: 50.0, right: 20.0)
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
+
+// Source help: http://www.raywenderlich.com/78550/beginning-ios-collection-views-swift-part-1
